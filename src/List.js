66ef6998ebe2b979ec6e95c641dfecb23d1946d1
Vardhan Goud <vardhangoud41@gmail.com>
2022-10-18 22:40:15 +0530

    added another component

---
+++ src/List.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function List() {
+  return (
+    <div>
+      <ol>
+        <li><a href="https//www.amazon.com">amazon</a></li>
+        <li><a href="https//www.google.com">Google</a></li>
+      </ol>
+    </div>
+  )
+}
+
+export default List
