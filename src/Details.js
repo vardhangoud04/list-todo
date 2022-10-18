f30024494d0cbe17e99f30dd638011e1120404be
Vardhan Goud <vardhangoud41@gmail.com>
2022-10-18 22:25:19 +0530

    added new Details component

---
+++ src/Details.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Details() {
+  return (
+    <div>
+      <h2>Hello user</h2>
+      <h4>These are the details of your Login</h4>
+    </div>
+  )
+}
+
+export default Details
